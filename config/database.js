cf3a6c22435850fd4bb32477bb1ee82255630df5
Luiz Varela <luiz.lvarela@gmail.com>
2014-03-14 20:07:01 -0300

    update db url

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-	url : 'mongodb://localhost/sales'
+	//url : 'mongodb://localhost/sales'
+	//url : 'mongodb://banco:banco123@ds033419.mongolab.com:33419/public_sales'
+	url : 'mongodb://nodejitsu_luizvarela666:54i6fksskci4rj1n1tr3rbajr7@ds061548.mongolab.com:61548/nodejitsu_luizvarela666_nodejitsudb6175208890'
 }
