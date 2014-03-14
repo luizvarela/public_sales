6986e8992fd97e186b87e942fad595290b100a10
Luiz Varela <luiz.lvarela@gmail.com>
2014-03-14 19:44:57 -0300

    Create products model

--- models/product.js
+++ models/product.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Product', {
-	name        : String,
-	description : String
+	nome           : String,
+	valor          : Number,
+	descricao      : String,
+	tipo_pagamento : Number,
+	categoria      : String,
+	valor_inicial  : Number,
+	valor_final    : Number,
+	tempo_leilao   : Date, 
+	data_inicial   : Date,
 });
